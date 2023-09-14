697d2519a2f3301e8c531c6d1e8c237b522328a3
Diana <132330171+DianaShuvalova@users.noreply.github.com>
2023-09-14 09:46:39 +0300

    Add files via upload

---
+++ 14.09_Задача про делители.py
@@ -0,0 +1,17 @@
+def deliteli(n: int) -> list:
+    lst = []
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            lst.append(i)
+            if i != n // i:
+                lst.append(n // i)
+    return sorted(lst)[::-1]
+
+
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    if len(deliteli(i)) == 4:
+        print(i)
+        print(*deliteli(i))
+        print()
