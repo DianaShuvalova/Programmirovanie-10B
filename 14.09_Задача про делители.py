c8767c9698f22a89f2bb55e260df688639445e43
Diana <132330171+DianaShuvalova@users.noreply.github.com>
2023-09-14 09:57:49 +0300

    Update 14.09_Задача про делители.py

--- 14.09_Задача про делители.py
+++ 14.09_Задача про делители.py
@@ -1,4 +1,5 @@
 def deliteli(n: int) -> list:
+    #function, which count and return the list of dividers of n
     lst = []
     for i in range(1, int(n ** 0.5) + 1):
         if n % i == 0:
@@ -7,10 +8,10 @@ def deliteli(n: int) -> list:
                 lst.append(n // i)
     return sorted(lst)[::-1]
 
-
+#checking type of input a, b
 while True:
     a, b = input(), input()
-    if a.isdigit() and b.isdigit():
+    if a.isdigit() == True and b.isdigit() == True:
         a, b = int(a), int(b)
 for i in range(a, b + 1):
     if len(deliteli(i)) == 4:
